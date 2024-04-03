680f0fe163f44996972c4f19af9c436535304902
Lobo <lobo.rodrigoalejandro.2020@gmail.com>
2024-04-03 09:14:11 -0300

    correccion

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -1,63 +1,63 @@
 #include <stdio.h>
 
 //a
-int cuadradoNumero(int x){
-    return x*x;
+int cuadradoNumero(int *x){
+    return *x * *x;
 }
 
 //b
-void cuadrado(int x){
-    printf("\n%d", x*x);
+void cuadrado(int *x){
+    printf("\n%d", *x * *x);
 }
 
 //c
-void mostrarValorYcontenido(int x){
-    printf("\nel valor es: %d", x);
+void mostrarValorYcontenido(int *x){
+    printf("\nel valor es: %d", *x);
     printf("\nla direccion de memoria es: %d", &x);
 }
 
 //d
-void invertir(int a, int b){
+void invertir(int *a, int *b){
     int aux;
-    aux=a;
-    a=b;
-    b=aux;
-    printf("\na invertido es: %d", a);
-    printf("\nb invertido es: %d", b);
+    aux=*a;
+    *a=*b;
+    *b=aux;
+    printf("\na invertido es: %d", *a);
+    printf("\nb invertido es: %d", *b);
 }
 
 //e
-void ordenar(int a, int b){
-    if (a<b)
+void ordenar(int *a, int *b){
+    if (*a<*b)
     {
-        printf("\na: %d", a);
-        printf("\nb: %d", b);
+        printf("\na: %d", *a);
+        printf("\nb: %d", *b);
     } else
     {
         int aux;
-        aux=a;
-        a=b;
-        b=aux;
-        printf("\na: %d", a);
-        printf("\nb: %d", b);
+        aux=*a;
+        *a=*b;
+        *b=aux;
+        printf("\na: %d", *a);
+        printf("\nb: %d", *b);
     }
 }
 
-void leerEimprimir(int a, int b){
+void leerEimprimir(int *a, int *b){
     //int aux1=1, aux2=1;
-    a = cuadradoNumero(a);
-    b = cuadradoNumero(b);
-    for (int i = 1; i < a; i++)
+    *a = cuadradoNumero(a);
+    *b = cuadradoNumero(b);
+    for (int i = 1; i < *a; i++)
     {
-        if (i*i==a)
+        if (i*i==*a)
         {
             printf("\nel valor de a: %d", i);
             break;
         }
     }
-    for (int j = 1; j < b; j++)
+    for (int j = 1; j < *b; j++)
     {
-        if (j*j==b)
+        if (j*j==*b)
         {
             printf("\nel valor de b: %d", j);
             break;
@@ -67,17 +67,22 @@ void leerEimprimir(int a, int b){
 }
 
 int main(){
-    int a=5;
-    int b=7;
-    int c;
+    int a, b, c;
+    printf("\ningrese el primer numero");
+    scanf("%d", &a);
+    printf("\ningrese el segundo numero");
+    scanf("%d", &b);
+    int *puntA = &a;
+    int *puntB = &b;
 
-    c = cuadradoNumero(a);
+
+    c = cuadradoNumero(puntA);
     printf("\n%d", c);
-    cuadrado(a);
-    mostrarValorYcontenido(a);
-    invertir(a,b);
-    ordenar(a,b);
-    leerEimprimir(a,b);
+    cuadrado(puntA);
+    mostrarValorYcontenido(puntA);
+    invertir(puntA,puntB);
+    ordenar(puntA,puntB);
+    leerEimprimir(puntA,puntB);
     printf("\n");
 
 
